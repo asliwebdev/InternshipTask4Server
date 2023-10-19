6cebde9f3aa806daccebcccf2ec297e35099275b
Asliddin Bozorov <117100602+asliwebdev@users.noreply.github.com>
2023-10-20 00:28:46 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://intern-task4.onrender.com'],
+    origin: ['http://localhost:5173', 'https://internship-task4.onrender.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
